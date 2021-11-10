19ea3c605b701121f6b191f0f81ecb20f2c26ddc
Denis Urías <78060754+DenisRodriguez-17@users.noreply.github.com>
2021-11-09 23:45:57 -0600

    bookwise

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import dataUsers from './dataUsers.js';
 
 //app config
 const app = express();
-const port = process.env.port || 8081;
+const port = process.env.port || 5000;
 
 //middlewares
 app.use(express.json());
