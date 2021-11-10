600ebd9595cce586d54d7361d3b3822042693480
Denis Urías <78060754+DenisRodriguez-17@users.noreply.github.com>
2021-11-09 23:50:36 -0600

    bookwsie

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import dataUsers from './dataUsers.js';
 
 //app config
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.port || 3001;
 
 //middlewares
 app.use(express.json());
